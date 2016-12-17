f3ac238b6ecd337f6235dbea87287e89418e0c62
Zeeshan Khan <zkhan19093@gmail.com>
2016-12-17 17:31:57 +0530

    added status

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Barackpore R.S Success Institute'
+	return 'Barackpore R.S Success Institute\nSite under construction'
 
 if __name__ == '__main__':
 	app.run()
