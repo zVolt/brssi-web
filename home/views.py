b30ff792cca5147383bf986d743edffb210e4159
Zeeshan Khan <zkhan19093@gmail.com>
2016-12-26 01:38:49 +0530

    refacto : layout change in index and added couple more pages
    todo:
    change drop down background-color via css

--- home/views.py
+++ home/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request,'base.html')
+	print 'index'
+	return render(request,'home/index.html')
+
+def disclaimer(request):
+	print 'disclaimer'
+	return render(request,'home/disclaimer.html')
