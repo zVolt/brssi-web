c2d41d5b4f021fa85b4fd556f206b85544c4b9ba
Zeeshan Khan <zkhan19093@gmail.com>
2016-12-28 01:50:33 +0530

    refacto added migrations

--- home/models.py
+++ home/models.py
@@ -2,28 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Student(models.Model):
-	GENDER_CHOICES=(
-		('Male','male'),
-		('Female','female'),
-		('Other','other')
-	)
-	name=models.CharField(max_length=100)
-	mothers_name=models.CharField(max_length=100)
-	date_of_birth=models.DateField(auto_now=True)
-	gender=models.ChoiceField(choices=GENDER_CHOICES)
-	address=models.TextField()
-	joined_on=models.DateTimeField(auto_now=True)
-
-class Faculty(models.Model):
-	name=models.CharField(max_length=100)
-	address=models.TextField()
-	contact_number=models.CharField(max_length=13)
-	email_address=models.EmailField()
-	high_school=models.ForeignKey(Qualification)
-	intermediate_school=models.ForeignKey(Qualification)
-	highest_qualification=models.ForeignKey(Qualification)
-
 
 class Subject(models.Model):
 	name=models.CharField(max_length=100)
@@ -34,3 +12,27 @@ class Qualification(models.Model):
 	school=models.CharField(max_length=100)
 	board=models.CharField(max_length=100)
 	result=models.CharField(max_length=100)
+	
+class Student(models.Model):
+	GENDER_CHOICES=(
+		('Male','male'),
+		('Female','female'),
+		('Other','other')
+	)
+	name=models.CharField(max_length=100)
+	mothers_name=models.CharField(max_length=100)
+	date_of_birth=models.DateField(auto_now=True)
+	gender=models.CharField(max_length=10,choices=GENDER_CHOICES)
+	address=models.TextField()
+	joined_on=models.DateTimeField(auto_now=True)
+
+class Faculty(models.Model):
+	name=models.CharField(max_length=100)
+	address=models.TextField()
+	contact_number=models.CharField(max_length=13)
+	email_address=models.EmailField()
+	high_school=models.ForeignKey(Qualification,related_name='high_school')
+	intermediate=models.ForeignKey(Qualification,related_name='intermediate')
+	highest_qualification=models.ForeignKey(Qualification,related_name='highest_qualification')
+
+
