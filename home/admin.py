644e1d5071bc9fed5708e369fc1004d649f96148
Zeeshan Khan <zkhan1093@gmail.com>
2016-12-29 23:31:41 +0530

    added some models

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-
+admin.site.register(Notice)
+admin.site.register(Branch)
+admin.site.register(Board)
 admin.site.register(Student)
 admin.site.register(Qualification)
 admin.site.register(Subject)
--- home/models.py
+++ home/models.py
@@ -2,7 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Notice(models.Model):
+	content=models.TextField()
+	created_on=models.DateTimeField(auto_now=True)
 
+class Branch(models.Model):
+	name=models.CharField(max_length=100)
+	
+class Board(models.Model):
+	name=models.CharField(max_length=100)
+	
 class Subject(models.Model):
 	name=models.CharField(max_length=100)
 	description=models.TextField()
@@ -19,11 +28,32 @@ class Student(models.Model):
 		('Female','female'),
 		('Other','other')
 	)
+	CLASS_CHOICES=(
+		('1','I'),
+		('2','II'),
+		('3','III'),
+		('4','IV'),
+		('5','V'),
+		('6','VI'),
+		('7','VII'),
+		('8','VIII'),
+		('9','IX'),
+		('10','X'),
+		('11','XI'),
+		('12','XII'),
+	)
 	name=models.CharField(max_length=100)
 	mothers_name=models.CharField(max_length=100)
-	date_of_birth=models.DateField(auto_now=True)
+	date_of_birth=models.DateField()
 	gender=models.CharField(max_length=10,choices=GENDER_CHOICES)
 	address=models.TextField()
+	contact_number=models.CharField(max_length=13)
+	email_address=models.EmailField()
+	school_name=models.CharField(max_length=100)
+	school_board=models.ForeignKey(Board)
+	school_class=models.CharField(max_length=5,choices=CLASS_CHOICES)
+	subjects=models.ManyToManyField(Subject)
+	prefered_branch=models.ForeignKey(Branch)
 	joined_on=models.DateTimeField(auto_now=True)
 
 class Faculty(models.Model):
