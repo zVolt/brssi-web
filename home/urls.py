f1062ca829b067f074a4125be4ea32fffbe5f2c6
Zeeshan Khan <zkhan19093@gmail.com>
2017-01-16 15:56:14 +0530

    mergin with master

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.user_login, name='login'),
+    url(r'^student/$', views.student, name='login'),
+    url(r'^faculty/$', views.faculty, name='login'),
+    url(r'^moderator/$', views.moderator, name='login'),
     url(r'^disclaimer/$', views.disclaimer, name='disclaimer'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Notice, Testimonial
 from django.contrib.auth import login,authenticate
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required,user_passes_test
 
+def is_student(user):
+    return user in Group.objects.get(name='students').user_set.all()
+
+
+def is_faculty(user):
+    return user in Group.objects.get(name='faculties').user_set.all()
+
+
+def is_admin(user):
+    return user in Group.objects.get(name='admin').user_set.all()
+
+def redirectUser(request):
+    if request.user in Group.objects.get(name='students').user_set.all():
+        return redirect('/student/')
+    elif request.user in Group.objects.get(name='faculties').user_set.all():
+        return redirect('/faculty/')
+    elif request.user in Group.objects.get(name='admin').user_set.all():
+        return redirect('/moderator/')
+    else:
+        data['result']=False
+        data['msg']='Cannot recognize you'
+        return render(request,'home/login.html',data)
 
 def index(request):
     data=dict()
@@ -17,22 +40,34 @@ def disclaimer(request):
 
 def user_login(request):
     data=dict()
+    if request.user.is_authenticated():
+        return redirectUser(request)
     if request.method=='POST':
         username=request.POST['userid']
         password=request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request,user)
-            if user in Group.objects.get(name='students').user_set.all():
-                return render(request,'home/student.html',data)
-            elif user in Group.objects.get(name='faculties').user_set.all():
-                return render(request,'home/faculty.html',data)
-            elif user in Group.objects.get(name='admin').user_set.all():
-                return render(request,'home/admin.html',data)
-            else:
-                data['result']=False
-                data['msg']='Cannot recognize you'
+            redirectUser(request)
         else:
             data['result']=False
             data['msg']='Username and Password combination is incorrect! Try again.'
     return render(request,'home/login.html',data)
+
+@login_required
+@user_passes_test(is_student)
+def student(request):
+    data=dict()
+    return render(request,'home/student.html',data)
+
+@login_required
+@user_passes_test(is_faculty)
+def faculty(request):
+    data=dict()
+    return render(request,'home/faculty.html',data)
+
+@login_required
+@user_passes_test(is_admin)
+def moderator(request):
+    data=dict()
+    return render(request,'home/moderator.html',data)
